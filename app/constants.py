1de6477a9cad56cbbf5d971b1b1570624859216d
Mikhail Dhruv <mikhail@Mikhails-MacBook-Pro.local>
2021-04-24 16:13:08 -0700

    updated raider details

---
+++ app/constants.py
@@ -0,0 +1,3 @@
+Classes = ["Mage", "Warlock", "Priest", "Druid", "Rogue", "Hunter", "Shaman", "Paladin", "Warrior"]
+Roles = ["Tank", "Healer", "Melee DPS", "Ranged DPS"]
+Professions = ["None","Tailoring", "Leathworking", "Blacksmithing", "Enchanting", "Alchemy", "Herbalist", "Mining", "Skinning", "Engineering", "Jewelcrafting"]
